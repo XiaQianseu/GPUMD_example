b86c283f0b33f5157c13e1ac789a7947ffeb071f
XiaQianseu <yn_xiaqian@163.com>
2026-07-04 00:45:44 +0800

    Add files via upload

---
+++ 03md/gpumd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -J dp  #作业名
+#SBATCH -p hgpu8
+#SBATCH --cpus-per-task=1  #cpu核心
+#SBATCH --gres=gpu:1  #GPU数量
+
+
+module load cuda/11.5
+module load gcc/7.5.0
+export PATH=/data1/home/hhu01/jieji/GPUMD/GPUMD-master/src:$PATH
+gpumd > log
